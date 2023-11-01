3f4291df1f406c087e5832684e9e64e4b2c77a7c
sparsh Kadian <sparshkadian323@gmail.com>
2023-11-01 10:11:32 +0530

    Update weatherController.js

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -28,8 +28,4 @@ exports.getData = async (req, res) => {
   }
 };
 
-// A quick fix to avoid server spin down
-setInterval(async () => {
-  const res = await axios.post('https://weather-wise.onrender.com/delhi');
-  const data = await res.data;
-}, 840000);
+
